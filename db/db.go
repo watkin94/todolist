cb5ec8cf3815f80d74417fa109d58f7ff7bc58ec
liweijian <watkin94@sina.com>
2023-08-17 10:16:00 +0800

    feat:添加环境变量

--- db/db.go
+++ db/db.go
@@ -1,8 +1,11 @@
 package db
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+	"os"
 	"runtime"
 )
 
@@ -12,6 +15,15 @@ var (
 
 func Connect() {
 	//这里尝试连接mysql
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	//使用环境变量
+	APP_ENV := os.Getenv("APP_ENV")
+	fmt.Println(APP_ENV)
+
 	var dns string
 	if runtime.GOOS == "windows"{
 		dns ="root:123456@tcp(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
